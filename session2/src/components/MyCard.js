c582c3361a0b0167021aec6dfc1e0a150a1454db
Devansh Jatin Ponda <devanshponda@gmail.com>
2021-10-18 14:40:54 -0400

    added empty card component

---
+++ session2/src/components/MyCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class MyCard extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
+
+export default MyCard
